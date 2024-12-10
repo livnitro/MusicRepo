e773bb91af141d36ee2d4547468a22954227a988
HazielDev <hazieldev@gmail.com>
2024-12-09 20:12:42 -0600

    Primer commit

--- app/src/main/java/com/example/musicrepo/presentation/ui/screens/Instruments.kt
+++ app/src/main/java/com/example/musicrepo/presentation/ui/screens/Instruments.kt
@@ -1,2 +1,19 @@
 package com.example.musicrepo.presentation.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun IntrumentsScreen(){
+    Column {  }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = false
+)
+@Composable
+fun InstrumentsScreenPreview(){
+    IntrumentsScreen()
+}
